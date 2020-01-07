034fc4fcebce4703aeedded584f4fb41cde12635
Lucas <luka_182@hotmail.es>
2020-01-07 00:23:40 -0300

    validacion usuario logeado

--- controladores/controladorHeader.php
+++ controladores/controladorHeader.php
@@ -5,6 +5,19 @@ if(isset($_SESSION["conectado"]) && $_SESSION["conectado"] == true){
     setcookie("log", true);
 }
 
+if(isset($_COOKIE['log']) && $_COOKIE['log'] == true){
+    if (strpos($_SERVER["PHP_SELF"], 'login') || strpos($_SERVER["PHP_SELF"], 'registro') ){
+        header('location: usuario.php');
+    }
+
+}
+
+if(!isset($_COOKIE['log']) ){
+    if (strpos($_SERVER["PHP_SELF"], 'carrito') ){
+        header('location: login.php');
+    }
+}
+
 if(isset($_POST['nombre_del_boton']) && $_POST['nombre_del_boton'] == "Salir"  )
 {
     $_SESSION['conectado']=false;
